54ce89710bab4c0e178578239f7f1b4d3ce6747f
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-07-25 08:50:19 +0800

    added a new commenys for documentation

--- checmical_diffusion.py
+++ checmical_diffusion.py
@@ -10,12 +10,12 @@ time_steps = np.arange(0, 9, 2)
 
 num_rows = len(time_steps) # 5
 num_cols = len(space_grid) # 5 
-ppm_matrix = np.zeros((num_rows, num_cols)) # to create a table or matric (5 columns x 5 rows)
+ppm_matrix = np.zeros((num_rows, num_cols)) # to create a table or matrix (5 columns x 5 rows)
 
 # Initial State at t = 0s
 # Node 0 is 80 ppm, rest are 0 ppm
 initial_ppm = np.array([80.0, 0.0, 0.0, 0.0, 0.0])
-ppm_matrix[0, :] = initial_ppm
+ppm_matrix[0, :] = initial_ppm # from index 0 up to the end
 
 diffusion_rate = 0.20
 
